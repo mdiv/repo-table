8cb60cd12d1b48f3631e3e25728c9a78837f4a04
cscott <cscott@twigeducation.com>
2022-08-30 10:31:35 +0100

    Add apollo client and configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <div>App</div>
+    <ApolloProvider client={client}>
+      <div>App</div>
+    </ApolloProvider>
   </React.StrictMode>,
 );
