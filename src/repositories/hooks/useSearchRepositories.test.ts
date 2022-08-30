5941f1d038f93b64605c701014e6e8353857d59d
cscott <cscott@twigeducation.com>
2022-08-30 13:15:22 +0100

    Fix search repos hook test.

--- src/repositories/hooks/useSearchRepositories.test.ts
+++ src/repositories/hooks/useSearchRepositories.test.ts
@@ -1,7 +1,7 @@
 import { encodeCursor } from "./useSearchRepositories";
 
 test("converts number to cursor", () => {
-  expect(encodeCursor(0)).toBe("Y3Vyc29yOjE=");
+  expect(encodeCursor(0)).toBe("Y3Vyc29yOjA=");
   expect(encodeCursor(1)).toBe("Y3Vyc29yOjE=");
   expect(encodeCursor(10)).toBe("Y3Vyc29yOjEw");
 });
